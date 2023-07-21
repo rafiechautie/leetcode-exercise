6189ec0a9ea6f98073d15387e9d94e2cb930911b
Muhammad Rafie Chautie <58112973+rafiechautie@users.noreply.github.com>
2023-07-21 21:26:54 +0700

    Time: 89 ms, Memory: 51.4 MB - LeetHub

---
+++ rotate-array/rotate-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const len = nums.length
+    k = (k % len)
+    
+    let end = nums.splice(len - k)
+
+    nums.splice(0,0,...end)
+};
+
+
